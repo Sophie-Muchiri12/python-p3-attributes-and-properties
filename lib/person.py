7802c2c2fc2cae8230d8603aa33475824033eca4
Sophie Muchiri <muthonisophie12@gmail.com>
2024-09-02 15:53:45 +0300

    attributes and properties

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+
+
+class Person:
+
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -15,5 +19,34 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+    def __init__(self,name="sophie",job="Marketing"):
+        self.name = name
+        self.job = job
+
+
+    @property
+    def name(self):
+        return self._name 
+
+    
+    @name.setter
+    def name(self,name):
+        if isinstance(name,str) and 1<= len(name) <= 25:
+            self._name = name.title()
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+
+    @property
+    def job(self):
+        return self._job
+
+    
+    @job.setter
+    def job(self,job):
+        if job in Person.APPROVED_JOBS:
+            self._job = job
+
+        else:
+            print( "Job must be in list of approved jobs." )
